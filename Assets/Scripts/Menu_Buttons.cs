77f6596fcaacd390d9cd9c6ee4428dac344fc315
chlovers <chloie_day@hotmail.com>
2026-01-18 18:41:24 -0400

    added button fuctions

--- Assets/Scripts/Menu_Buttons.cs
+++ Assets/Scripts/Menu_Buttons.cs
@@ -1,16 +1,51 @@
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu_Buttons : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void Night()
     {
-        
+        SceneManager.LoadScene("Night");
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Day()
     {
-        
+        SceneManager.LoadScene("Day");
+
     }
+
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if (SceneManager.GetActiveScene().name == "Night")
+            {
+                SceneManager.LoadScene("Day");
+
+
+            }
+
+            else 
+            {
+                SceneManager.LoadScene("Night");
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        
+        }
+
+    }
+
 }
